c7cc9843ecb77528ec0373a5dc868b9a9783ece9
Rafi Baum <rafi@ukbaums.com>
2020-06-02 23:04:09 -0400

    Implemented PNG output

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,23 @@ const IMAGE_WIDTH: u32 = 256;
 const IMAGE_HEIGHT: u32 = 256;
 
 fn main() {
-    print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+    let mut buf = image::ImageBuffer::new(IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    for j in (0..IMAGE_HEIGHT).rev() {
+    for j in 0..IMAGE_HEIGHT {
+        println!("Scanlines remaining: {}", IMAGE_HEIGHT - j);
         for i in 0..IMAGE_WIDTH {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let g = (IMAGE_HEIGHT - j - 1) as f64 / (IMAGE_HEIGHT - 1) as f64;
             let b = 0.25;
 
-            let ir = (255.999 * r) as u32;
-            let ig = (255.999 * g) as u32;
-            let ib = (255.999 * b) as u32;
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
 
-            print!("{} {} {}\n", ir, ig, ib);
+            buf.put_pixel(i, j, image::Rgb([ir, ig, ib]));
         }
     }
+
+    buf.save("image.png").unwrap();
+    println!("Done!");
 }
