bb4bd7120827a921c447d2f63124e2ba7d57f42e
Rafi Baum <rafi@ukbaums.com>
2020-06-09 17:36:05 -0400

    Implemented a basic red sphere

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,20 @@ fn main() {
 }
 
 fn ray_color(ray: &Ray) -> Vec3 {
+    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, ray) {
+        return Vec3::new(1.0, 0.0, 0.0);
+    }
+
     let unit_dir = ray.direction.unit_vector();
-    let t = (unit_dir.y + 1.0) * 0.5;
+    let t = 0.5 * (unit_dir.y + 1.0);
     Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
+}
+
+fn hit_sphere(center: &Vec3, radius: f64, ray: &Ray) -> bool {
+    let oc = ray.origin - *center;
+    let a = ray.direction.dot(&ray.direction);
+    let b = 2.0 * oc.dot(&ray.direction);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
 }
